9d84987e56bb2b867c651fed01969c5db753e470
LizaAgey <agey.eliz@gmail.com>
2022-04-25 21:39:34 +0300

    feat: burger partial functional

--- assets/files/script.js
+++ assets/files/script.js
@@ -1,1 +1,42 @@
-console.log("hi")
+const BURGER = document.querySelector('.burger')
+const SEC_INTRODUCTION = document.querySelector('.section-introduction')
+
+
+BURGER.addEventListener('click', (event) => {
+
+    SEC_INTRODUCTION.classList.toggle('section-introduction--active-nav')
+    BURGER.classList.toggle('burger--click')
+
+    if (SEC_INTRODUCTION.classList.contains('section-introduction--active-nav')) {
+        hideScroll()
+    } else {
+        showScroll()
+    }
+
+})
+
+const hideScroll = () => {
+    document.body.style.paddingRight = `${getScrollBarWidth()}px`
+    document.body.style.overflow = 'hidden'
+}
+const showScroll = () => {
+    document.body.style.paddingRight = ''
+    document.body.style.overflow = 'visible'
+}
+
+const getScrollBarWidth = () => {
+    const outer = document.createElement('div')
+
+    outer.style.position = 'absolute'
+    outer.style.top = '-9999px'
+    outer.style.width = '50px'
+    outer.style.height = '50px'
+    outer.style.overflow = 'scroll'
+    outer.style.visibility = 'hidden'
+
+    document.append(outer)
+    const scrollBarWidth = outer.offsetWidth - outer.clientWidth
+    document.body.remove(outer)
+    
+    return scrollBarWidth
+}
