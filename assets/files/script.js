4ec509b2a4bf5470ccb4671f3fd3fa0ba3db3c5a
LizaAgey <agey.eliz@gmail.com>
2022-04-26 14:37:08 +0300

    feat: burger menu main page is added

--- assets/files/script.js
+++ assets/files/script.js
@@ -1,26 +1,42 @@
 const BURGER = document.querySelector('.burger')
+const BURGER_RIGHT = document.querySelector('.header-burger-right-menu')
 const SEC_INTRODUCTION = document.querySelector('.section-introduction')
+const HEADER = document.querySelector('header')
+const BODY = document.querySelector('body')
+const NAV_WRAPPER = document.querySelector('.nav-wrapper')
+const RIGHT_MENU = document.querySelector('.right-menu')
 
 
-BURGER.addEventListener('click', (event) => {
+BURGER.addEventListener('click', () => {
 
     SEC_INTRODUCTION.classList.toggle('section-introduction--active-nav')
-    BURGER.classList.toggle('burger--click')
+    
 
     if (SEC_INTRODUCTION.classList.contains('section-introduction--active-nav')) {
-        hideScroll()
+        hideScroll();
     } else {
-        showScroll()
+        showScroll();
     }
 
 })
 
+BURGER_RIGHT.addEventListener('click', () => {
+    SEC_INTRODUCTION.classList.toggle('section-introduction--active-nav')
+    
+
+    if (SEC_INTRODUCTION.classList.contains('section-introduction--active-nav')) {
+        hideScroll();
+    } else {
+        showScroll();
+    }
+})
+
 const hideScroll = () => {
-    document.body.style.paddingRight = `${getScrollBarWidth()}px`
+    document.body.style.paddingRight = `${getScrollBarWidth()}px`;
     document.body.style.overflow = 'hidden'
 }
 const showScroll = () => {
-    document.body.style.paddingRight = ''
+    document.body.style.paddingRight = '';
     document.body.style.overflow = 'visible'
 }
 
@@ -34,9 +50,16 @@ const getScrollBarWidth = () => {
     outer.style.overflow = 'scroll'
     outer.style.visibility = 'hidden'
 
-    document.append(outer)
+    document.body.appendChild(outer)
     const scrollBarWidth = outer.offsetWidth - outer.clientWidth
-    document.body.remove(outer)
-    
+    document.body.removeChild(outer)
+
     return scrollBarWidth
 }
+
+const resetNav = () => {
+    SEC_INTRODUCTION.classList.remove('section-introduction--active-nav')
+    showScroll()
+}
+
+window.addEventListener('resize', resetNav)
