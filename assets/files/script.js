8459545127f474d43c046934fdef0ae6aaa0111c
LizaAgey <agey.eliz@gmail.com>
2022-04-28 12:47:00 +0300

    fix: update pets.json path_

--- assets/files/script.js
+++ assets/files/script.js
@@ -100,7 +100,7 @@ window.addEventListener('mouseup', function (event) {
 
 //  === SLIDER  ===
 
-const petsJSON = './shelter-rs-app/assets/files/pets.json';
+const petsJSON = './assets/files/pets.json';
 
 
 
